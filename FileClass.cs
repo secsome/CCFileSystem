76c7b8bed84e8bd9f5d81fb93f082461a1dd07f9
secsome <302702960@qq.com>
2022-12-16 14:22:07 +0800

    Add FileClass abstract class

---
+++ FileClass.cs
@@ -0,0 +1,32 @@
+namespace CCFileSystem
+{
+	public abstract class FileClass
+	{
+		public abstract string FileName();
+
+		public abstract string SetName(string filename);
+
+		public abstract bool Create();
+
+		public abstract bool Delete();
+
+		public abstract bool IsAvailable(bool forced = false);
+
+		public abstract bool IsOpen();
+
+		public abstract bool Open(string filename, FileAccess rights = FileAccess.Read);
+
+		public abstract bool Open(FileAccess rights = FileAccess.Read);
+
+		public abstract byte[]? Read(long length);
+
+		public abstract long Seek(long pos, SeekOrigin dir = SeekOrigin.Current);
+
+		public abstract long Size();
+
+		public abstract long Write(byte[] buffer);
+
+		public abstract void Close();
+
+	}
+}
